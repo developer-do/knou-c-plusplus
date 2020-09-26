9ace4242a890b189e7d52b3ed0b0a292365b03f4
dev-do <dev-do@naver.com>
2020-09-27 02:49:16 +0900

    c++ 6강 클래스와 객체 (2) 공부오나료

--- 6강/Project/Project/VFMain1.cpp
+++ 6강/Project/Project/VFMain1.cpp
@@ -5,11 +5,16 @@ using namespace std;
 
 int main() {
     float a[3] = { 1,2,3 };
+    float b[3] = { 2, 4, 6 };
     VecF v1(3, a); // 1,2,3을 저장하는 벡터
-    VecF v2(v1);   // v1을 복사하여 v2를 만듦
+    VecF v2(3, b);   // v1을 복사하여 v2를 만듦
+    VecF v3(v1.add(v2));
+    VecF v4(v2.add(v3));
     v1.print();
     cout << endl;
-    //v2.print();
-    //cout << endl;
+    v2.print();
+    cout << endl;
+    v4.print();
+    cout << endl;
     return 0;
 }
--- 6강/Project/Project/VecF.h
+++ 6강/Project/Project/VecF.h
@@ -12,6 +12,12 @@ public:
         if (a) memcpy(arr, a, sizeof(float) * n);
     }
 
+    // ±Ì¿∫ ∫πªÁ
+    VecF(const VecF& fv) : n{ fv.n } {
+        arr = new float[n];
+        memcpy(arr, fv.arr, sizeof(float) * n);
+    }
+
     ~VecF() {
         delete[] arr;
     }
